abc4823a21cfa38cc80f0cd0b34004e87ac843fc
Laura Camacho <laura.camacho@ucb.edu.bo>
2024-11-07 06:47:43 -0400

    Prueba cypress pasando: sumar la cadena por delimitadores

--- cypress/e2e/sumaCadena.cy.js
+++ cypress/e2e/sumaCadena.cy.js
@@ -10,5 +10,11 @@ describe("Sumador", () => {
         cy.get("#cadena").type("1-2,3");
         cy.get("#calculadora-button").click();
         cy.get("#resultado-div").should("contain", 6);
-      });
+    });
+    it("Muestra la suma de la cadena por los separadores personalizados ingresada por el usuario", () => {
+        cy.visit("/");
+        cy.get("#cadena").type("//[;] 6;7;4");
+        cy.get("#calculadora-button").click();
+        cy.get("#resultado-div").should("contain", 17);
+    });
   });
