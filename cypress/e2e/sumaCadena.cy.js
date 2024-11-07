641cc2f067979b65cdf4f916d07777243f9e4dcf
Laura Camacho <laura.camacho@ucb.edu.bo>
2024-11-07 06:13:53 -0400

    Prueba cypress pasando: sumar la cadena por comas y guiones

--- cypress/e2e/sumaCadena.cy.js
+++ cypress/e2e/sumaCadena.cy.js
@@ -5,4 +5,10 @@ describe("Sumador", () => {
       cy.get("#calculadora-button").click();
       cy.get("#resultado-div").should("contain", 19);
     });
+    it("Muestra la suma de la cadena separada por guiones ingresada por el usuario", () => {
+        cy.visit("/");
+        cy.get("#cadena").type("1-2,3");
+        cy.get("#calculadora-button").click();
+        cy.get("#resultado-div").should("contain", 6);
+      });
   });
