482be67162a4c769afcab786754b2cd42c1d9b6f
Laura Camacho <laura.camacho@ucb.edu.bo>
2024-11-07 06:49:49 -0400

    Prueba jest  pasando: sumar la cadena por delimitadores

--- src/sumarcadena.js
+++ src/sumarcadena.js
@@ -1,7 +1,17 @@
 function calcularCadena(cadena) {
-    const numeros = cadena.split(/[,|-]/) 
-      .map(num => parseInt(num.trim(), 10)) 
-    const resultado = numeros.reduce((acc, num) => acc + num, 0);
+    const delimitadorMatch = cadena.match(/^\/\/\[(.*?)\]/);
+    let resultado = 0;
+    if (delimitadorMatch) {
+        const delimitadores = delimitadorMatch[1].split('][').map(d => d.trim());
+        const regexDelimitadores = new RegExp(`[${delimitadores.join('')}]`);
+        cadena = cadena.slice(delimitadorMatch[0].length).trim();
+        const numeros = cadena.split(regexDelimitadores).map(num => parseInt(num.trim(), 10));
+        resultado = numeros.reduce((acc, num) => acc + num, 0);
+
+    } else {
+        const numeros = cadena.split(/[,|-]/).map(num => parseInt(num.trim(), 10));
+        resultado = numeros.reduce((acc, num) => acc + num, 0);
+    }
     return resultado;
   }
   
--- src/sumarcadena.test.js
+++ src/sumarcadena.test.js
@@ -7,4 +7,7 @@ describe("Calculadora de Cadena", () => {
   it("debería sumar los números en una cadena con comas y guiones", () => {
     expect(calcularCadena("4-5-6")).toEqual(15); 
   });
+  it("debería sumar los números en una cadena con delimitador personalizado *", () => {
+    expect(calcularCadena("//[*] 2*3*4")).toEqual(9); 
+  });
 });
